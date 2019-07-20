9d41fbc9ff46a633a41cf7efe3e042fdedf8ca78
fengchejuren <superman166@126.com>
2019-07-20 18:37:12 +0800

    分片上传超大文件

--- src/main/java/com/qwg/webcontroller/FileController.java
+++ src/main/java/com/qwg/webcontroller/FileController.java
@@ -1,14 +1,18 @@
 package com.qwg.webcontroller;
 
 import com.qwg.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Administrator on 2019/6/10.
@@ -16,21 +20,44 @@ import java.util.Map;
 @Controller
 @RequestMapping("file")
 public class FileController {
+    private static Logger logger = LoggerFactory.getLogger(FileController.class);
 
     @Autowired
     private UserService userService;
 
+    @Value("${file.location}")
+    private String fileLocation;
+
     @RequestMapping("toUpload")
-    public String toUpload(Model model,String user_id){
+    public String toUpload(Model model, String user_id, HttpServletRequest request){
         HashMap user = userService.findById(user_id);
         model.addAttribute("name","张三");
         model.addAttribute("user",user);
         return "fileupload/upload";
     }
 
-    @PostMapping("largeFileUpload")
-    public String largeFileUpload(Model model,Map<String,Object> map){
-        model.addAttribute("name","张三");
-        return "fileupload/upload";
+    @RequestMapping(value = "/largeFileUpload", method = RequestMethod.POST)
+    @ResponseBody
+    public String largeFileUpload(@RequestParam("file") MultipartFile file, String index,String finished){
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(
+                        new FileOutputStream(new File(fileLocation + File.pathSeparator+
+                                file.getOriginalFilename())));
+                System.out.println(file.getName());
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            }
+            return "fileupload/upload";
+        } else {
+            return "fileupload/upload";
+        }
     }
 }
--- src/main/resources/static/js/jquery.file-upload-0.1.js
+++ src/main/resources/static/js/jquery.file-upload-0.1.js
@@ -6,8 +6,23 @@
       bytesPerPiece: 1024*1024*10,//单个文件大小:10M
       maxFileSize: 1024*1024*1024,   //总文件大小:1G
   };
+  var stateCode = '200';    //状态码:200-准备就绪,201-上传中,206-上传成功,207-上传失败
+  var substate = [];
+  var handlerFunction;
+  var completeFunc = function (result,handler) {
+      if(result.code == '10000'){
+          if(substate.every(value => value){
+              handler.call(window,result);
+          }
+      }
+  };
   $.fn.extend({
-      submitLargeFileForm: function (settings,handlerFunc) {
+      submitLargeFileForm: function (settings,handler) {
+          if(stateCode == '201'){
+              return false;
+          }
+          stateCode = '201';
+          handlerFunction = handler;
           Object.assign(_settings,settings);
           //获取表单中的文件
           var file;
@@ -23,34 +38,47 @@
           if(fileSize>_settings.maxFileSize){
               var result = {};
               result.code = '99999';
-              result.msg = '上传的文件不能超过'+(_settings.maxFileSize/1024/1024)+"M";
+              result.msg = '上传的文件不能超过'+(_settings.maxFileSize/1024/1024)+"MB";
               handlerFunc.call(window,result);
               return this;
           }
           var start = 0;
           var end=0;
           var index = 0;
-          while(start<fileSize){
-              if(start+_settings.bytesPerPiece<=fileSize){
+          while(start<fileSize && stateCode == '201'){
+              var fm = new FormData();
+              fm.set('index',index);
+              substate[index] = false;
+              if(start+_settings.bytesPerPiece<fileSize){
                   end = start+_settings.bytesPerPiece;
               }else{
                   end = fileSize;
+                  fm.set('finished','1');
               }
               var chunk = blobFile.slice(start,end);
-              var formData = new FormData(this);
-              formData.append("file","123");
+              fm.append("file",chunk,fileName+".part"+index);
               $.ajax({
-                  url: _settings.url,
-                  type: 'post',
-                  cache: false,
-                  data: {'file':'123'},
-                  success: function(result){
-                      handlerFunc.call(window,result);
-                      return this;
+                  'url': _settings.url,
+                  'type': 'POST',
+                  'cache': false,
+                  'data': fm,
+                  'processData': false,
+                  'contentType':false,
+                  'success': function(result){
+                      if(result.code == '10000'){
+                          substate[index] = true;
+                          if(substate.every(value=>value)){
+                              stateCode = '206';
+                              handler.call(window,result);
+                          }
+                      }else{
+                          stateCode = '207';
+                          handlerFunc.call(window,result);
+                      }
                   },
-                  fail: function (result) {
+                  'fail': function (result) {
+                      stateCode = '207';
                       handlerFunc.call(window,result);
-                      return this;
                   }
               });
               start = end;
